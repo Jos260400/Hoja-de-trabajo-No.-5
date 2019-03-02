fbd0342bd4da0f267543dc5ae7c95c3b9a5168d6
JoeBlock99 <40869458+JoeBlock99@users.noreply.github.com>
2019-03-01 17:42:40 -0800

    Add files via upload
    Cuañquier cosa me decis

---
+++ Laboratorio5.py
@@ -0,0 +1,53 @@
+
+import simpy
+import random
+from math import sqrt
+from statistics import stdev
+env = simpy.Environment()
+cpu = simpy.Resource(env, capacity=1)#Cambiar el cpu de 1 a 2
+ram = simpy.Container(env, capacity=100, init=100) # cambiar capacity e init a 200 para ram
+listaTiempo = []
+def proceso(env, cpu, ram, num_inst, num_ram):
+	#	pedir ram
+	print ("Numero de instruccion =",num_inst,"\n", "Ram Disponible =",num_ram,"\n","Ram Level =",ram.level,"\n")
+	tiempo_inicial = env.now
+	with ram.get(num_ram) as memoria:
+		yield memoria
+
+		while num_inst > 2:
+			#	pedir cpu
+			with cpu.request() as turno:
+				yield turno
+				#	esperar turno
+				yield env.timeout(1)
+				#	simular proceso
+				num_inst = num_inst - 3
+
+			#	devuelve cpu
+			r = random.randint(1,2)
+			if r == 2:
+				#	simula operaciones I/O
+				yield env.timeout(1)
+	ram.put(num_ram)
+	tiempo_final = env.now - tiempo_inicial
+	listaTiempo.append(tiempo_final)
+	print("Tiempo final=",tiempo_final,"\n")
+	
+	
+	#	devolver ram al finzalizar proceso
+
+def process_generator(env, cpu, ram):
+    proc = 25#cambiar de 25 a 50 a 100 a 200 para cada test 
+    for i in range(proc):
+        env.process(proceso(env, cpu, ram, random.randint(1, 10), random.randint(1, 10)))
+        yield env.timeout(random.expovariate(1/10))
+env.process(process_generator(env, cpu, ram))
+env.run()
+tiempoPromedio = sum(listaTiempo)/proc
+print("El tiempo promedio es: ",tiempoPromedio)
+
+#desviacion = stdev(tiempoPromedio)
+#print ("La desviacion estandard es ", desviacion)
+
+
+
